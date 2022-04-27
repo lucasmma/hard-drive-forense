dc761430539fb8acf63eb2be7143de9566ecfef7
Lucas Mendonça <lucasmmamaral@gmail.com>
2022-04-27 10:37:53 -0300

    feat: print directory of hard drive

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
+#include <string>
+#include <iostream>
+#include <dirent.h>
+
+void printDirectory (char* path){
+  if (auto dir = opendir(path)) {
+    while (auto f = readdir(dir)) {
+        if (!f->d_name || f->d_name[0] == '.')
+          continue; // Skip everything that starts with a dot
+        
+        if (f->d_type == DT_DIR ){
+          printf("Directory: %s\n", f->d_name);
+          /* code */
+        } else{
+          printf("File: %s\n", f->d_name);
+        }
+        
+    }
+    closedir(dir);
+  }
+}
 
 int main (int argc, char const **argv) {
-  
+  char* path = "/mnt/e";
+  printDirectory(path);
   return 0;
 }
